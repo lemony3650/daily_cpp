631fece47f214ded411d30eeaddf1d4ddda0c202
lemony <253398316@qq.com>
2020-06-25 09:45:26 +0800

    update

--- cpp STL/template_class.cpp
+++ cpp STL/template_class.cpp
@@ -4,10 +4,9 @@ using namespace std;
 
 /********************************************************************
  * 类模板，建立一个通用类对象 + 类
- * template<typename T>
- * class 
- * 类模板无法自动进行推导，必须指定类型，但类模板可以有默认参数类型
- * 类外实现的成员函数需要添加模板参数列表
+ * template<typename T> || class 
+   1. 类模板无法自动进行推导，必须指定类型，但可以有默认参数类型
+   2. 类外实现的成员函数需要添加模板参数列表
 ********************************************************************/
 template<typename NameType, typename AgeType = int>
 class Person
@@ -45,6 +44,7 @@ void test01()
 }
 
 /***********************************************
+ * 普通成员函数编译阶段就创建了
  * 类模板中成员函数只有在函数调用时才会创建
  * 如新建两个类，通过第三个模板类进行调用
 ************************************************/
@@ -91,7 +91,7 @@ void test02()
  * 类模板对象作为函数的参数实现，一般会选择第一种方式
  * 1.指定参数实例
  * 2.类模板参数,指定参数类型为模板 template<typename T1, typename T2>
- * 3.整个类模板化 template<typename T>
+ * 3.整个类对象当作模板参数 template<typename T>
 ********************************************************************/
 void printPerson1(Person<string,int> &p);
 
@@ -99,7 +99,7 @@ template<typename T1, typename T2>
 void printPerson2(Person<T1,T2> &P);
 
 template<typename T>
-void printPerson3(T &p);
+void printPerson3(T &p){};
 
 /********************************************************
  * 子类继承父类（类模板）时需要指定父类的参数类型T
@@ -111,11 +111,12 @@ class Base
 {
     T m;
 };
-// 父类必须指定类型
+// 父类必须指定类型 Base<int>
 class Son:public Base<int>
 {
 
 };
+
 // 模板子类继承模板父类
 template<typename T1, typename T2>
 class Son_1:public Base<T2>
@@ -141,29 +142,29 @@ Son_1<int,char> child;
  * 1.类内实现
  * 2.类外实现
 ****************************************************************************/
-//2、全局函数配合友元  类外实现 - 先做函数模板声明，下方在做函数模板定义，在做友元
-template<class T1, class T2> class Person;
+//2、全局函数配合友元  类外实现 
 
+// 先做函数模板声明，下方在做函数模板定义，在做友元
+template<typename T1, typename T2> 
+class Person;
 //如果声明了函数模板，可以将实现写到后面，否则需要将实现体写到类的前面让编译器提前看到
-//template<class T1, class T2> void printPerson2(Person<T1, T2> & p); 
-
-template<class T1, class T2>
+template<typename T1, typename T2>
 void printPerson2(Person<T1, T2> &p)
 {
 	cout << "类外实现 ---- 姓名： " << p.m_Name << " 年龄：" << p.m_Age << endl;
 }
 
-template<class T1, class T2>
+
+// 模板类
+template<typename T1, typename T2>
 class Person
 {
 	//1、全局函数配合友元   类内实现
-	friend void printPerson(Person<T1, T2> & p)
+    //类外实现  friend void printPerson2<>(Person<T1, T2> p);
+	friend void printPerson(Person<T1, T2> p)
 	{
 		cout << "姓名： " << p.m_Name << " 年龄：" << p.m_Age << endl;
 	}
-
-	//全局函数配合友元  类外实现
-	friend void printPerson2<>(Person<T1, T2> & p);
 
     public:
         Person(T1 name, T2 age)
@@ -180,7 +181,7 @@ class Person
 //1、全局函数在类内实现
 void test01()
 {
-	Person <string, int >p("Tom", 20);
+	Person <string, int>p("Tom", 20);
 	printPerson(p);
 }
 
