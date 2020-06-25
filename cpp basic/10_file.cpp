67d98b6eb6cd55c1cd3b9e06f29fc979a9fdfbe3
lemony <253398316@qq.com>
2020-06-25 08:50:51 +0800

    update

--- cpp basic/10_file.cpp
+++ cpp basic/10_file.cpp
@@ -6,15 +6,17 @@ using namespace std;
 /***********************************************
  * 1.文本文件 (Ascii码)
  * 2.二进制文件
- * ofstream | ifstream | fstream
+ * ofstream | ifstream | fstream 三种文件流
+ * 写          读          读写
 ************************************************/
 
-/*********************************************
- * ios::in | ios::out  | ios::ate
- * ios::app| ios::trunc| ios::binary
-**********************************************/
-void test()
+/********************************************************
+ * ios::in | ios::out                  | ios::ate 文件尾
+ * ios::app| ios::trunc 先删除再创建新的 | ios::binary
+*********************************************************/
+void test01()
 {
+    // 创建流对象 写
     ofstream ofs;
 
     ofs.open("filename.txt",ios::out);
@@ -22,10 +24,13 @@ void test()
     ofs << "write data to ofs" << endl;
 
     ofs.close();
+
 }
+
 void test02()
 {
     ifstream ifs;
+
     char buf[1024] = {0};
     
     ifs.open("filename.txt",ios::in);
@@ -33,15 +38,16 @@ void test02()
     if (!ifs.is_open())
         cout << "打开文件失败" << endl;
 
-    // 1.读到字符数组中，以空格分隔
+    // 1.读到字符数组中，以换行分隔
     while(ifs >> buf)
         cout << buf << endl;
 
-    // 2.
+    // 2.getline   arg1: *buf   arg2: size
     while(ifs.getline(buf,sizeof(buf)))
         cout << buf << endl;
 
     // 3.从输出读到输入流
+    // 全局getline函数
     string buff;
     while(getline(ifs,buff))
         cout << buff << endl;
@@ -50,6 +56,8 @@ void test02()
     char ch;
     while ((ch = ifs.get())!=EOF)
         cout << ch << endl;
+
+    ifs.close();
 }
 
 
